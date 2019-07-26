1cdbd44bcf4e21e3842d5532697275b9bc1b9432
Malind Kowalski <malind.nikulski@gmail.com>
2019-07-26 22:23:51 +0000

    added Article and App components to code along with Learn Components lesson

---
+++ App.js
@@ -0,0 +1,13 @@
+// Step 2: Use the Component
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Article />
+      </div>
+    )
+  }
+}
+
+// line 7 is calling the Article Component within the App Component
---
+++ Article.js
@@ -0,0 +1,15 @@
+// Step1: Write the Component
+
+class Article extends React.Component {
+  render() {
+    return (
+      <div>
+        I am following along with the Learn Components lesson.  Not certain how sandbox works though.
+      </div>
+    )
+  }
+}
+
+// new class Article is declared
+// class extends React's Component class which provides built in methods/attributes
+// render() is defined and returns explicitly what is state in render({ return() })
